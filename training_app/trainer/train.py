25fcce8ebf554248375ba769de6fb1706cf0fb00
jarokaz <jarokaz>
2019-08-20 04:51:29 +0000

    Updates

--- training_app/trainer/train.py
+++ training_app/trainer/train.py
@@ -5,10 +5,10 @@ import subprocess
 import sys
 
 import fire
-import joblib
 import numpy as np
 import pandas as pd
 
+from sklearn.externals import joblib
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectKBest, chi2
@@ -20,9 +20,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
-def select_model(X, y, n_features_options, l2_reg_options):
+
+def train(job_dir, data_path, n_features_options, l2_reg_options):
     
-  # Set up grid search
+  # Load data from GCS
+  df_train = pd.read_csv(data_path)
+
+  y = df_train.octane
+  X = df_train.drop('octane', axis=1)
+    
   pipeline = Pipeline([
     ('scale', StandardScaler()),
     ('reduce_dim', PCA()),
@@ -31,35 +37,14 @@ def select_model(X, y, n_features_options, l2_reg_options):
 
   param_grid = [
     {
-        'reduce_dim': [PCA()],
-        'reduce_dim__n_components': n_features_options,
-        'regress': [Ridge()],
-        'regress__alpha': l2_reg_options
-    },
-    {
-        'reduce_dim': ['passthrough'],
-        'regress': [PLSRegression(scale=False)],
-        'regress__n_components': n_features_options
+      'reduce_dim__n_components': n_features_options,
+      'regress__alpha': l2_reg_options
     }
   ]
 
   grid = GridSearchCV(pipeline, cv=10, n_jobs=None, param_grid=param_grid, scoring='neg_mean_squared_error', iid=False)
-
+  
   grid.fit(X, y)
-
-  return grid
-
-def train(job_dir, data_path, n_features_options, l2_reg_options):
-    
-  # Load data from GCS
-  df_train = pd.read_csv(data_path, index_col=0)
-  y = df_train.octane
-  X = df_train.drop('octane', axis=1)
-    
-  # Find the best model
-  y = df_train.octane
-  X = df_train.drop('octane', axis=1)
-  grid = select_model(X, y, n_features_options, l2_reg_options)
 
   logging.info("Best estimator: {}".format(grid.best_params_))
   logging.info("Best score: {}".format(grid.best_score_))
@@ -73,8 +58,7 @@ def train(job_dir, data_path, n_features_options, l2_reg_options):
   joblib.dump(value=trained_pipeline, filename=model_filename)
   gcs_model_path = "{}/trained_model/{}".format(job_dir, model_filename)
   subprocess.check_call(['gsutil', 'cp', model_filename, gcs_model_path], stderr=sys.stdout)
-  logging.info("Saved model in: {}".format(gcs_model_path))
-  
+  logging.info("Saved model in: {}".format(gcs_model_path)) 
     
 if __name__ == "__main__":
   logging.basicConfig(level=logging.INFO)
